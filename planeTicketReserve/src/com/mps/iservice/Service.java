d7ad5705d3745dd8851896c8daff606e78059644
wangjian1995 <1175646958@qq.com>
2016-06-20 16:14:36 +0800

    save

--- planeTicketReserve/src/com/mps/iservice/Service.java
+++ planeTicketReserve/src/com/mps/iservice/Service.java
@@ -1,5 +1,6 @@
 package com.mps.iservice;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,6 +11,9 @@ public interface Service {
 	@GET
 	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
 	@Path("/test")
+	@Consumes({MediaType.APPLICATION_JSON})
 	public void test();
 	
+	
 }
+ 
