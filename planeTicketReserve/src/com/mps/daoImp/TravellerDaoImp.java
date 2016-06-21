01b06155405aa915fc6d7b3a3a60c635fdf66355
shipoyewu <871650575@qq.com>
2016-06-21 09:53:48 +0800

    订票页面完善

--- planeTicketReserve/src/com/mps/daoImp/TravellerDaoImp.java
+++ planeTicketReserve/src/com/mps/daoImp/TravellerDaoImp.java
@@ -1,9 +1,10 @@
 package com.mps.daoImp;
 
 import com.mps.daoBase.BaseDao;
+import com.mps.model.Traveller;
 
-public class TravellerDaoImp extends BaseDao<TravellerDaoImp, Integer> {
+public class TravellerDaoImp extends BaseDao<Traveller, Integer> {
 	public TravellerDaoImp(){
-		super(TravellerDaoImp.class);
+		super(Traveller.class);
 	}
 }
