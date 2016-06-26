11cdf5da5fe63fcf5b2fe090b33aa88412a415bf
shipoyewu <871650575@qq.com>
2016-06-26 08:38:43 +0800

    w

--- planeTicketReserve/src/JunitTest.java
+++ planeTicketReserve/src/JunitTest.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.apache.axis.Message;
 import org.apache.axis.message.MessageElement;
+import org.apache.cxf.wsdl.TTypes;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,7 +12,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.mps.daoImp.AgencyDaoImp;
 import com.mps.daoImp.OrdersDaoImp;
+import com.mps.daoImp.ParticipateDaoImp;
+import com.mps.daoImp.TeamDaoImp;
 import com.mps.daoImp.TravellerDaoImp;
+import com.mps.service.ServiceImp;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,9 +27,19 @@ public class JunitTest {
 	TravellerDaoImp travellerDaoImp;
 	@Resource
 	OrdersDaoImp ordersDaoImp;
+	@Resource
+	TeamDaoImp teamDaoImp;
+	@Resource
+	ParticipateDaoImp participateDaoImp;
+	
 	
 	@Test
 	public void test(){
-		System.out.println(ordersDaoImp.getCountOfAirline("asda", new Date()));
+		System.out.println("123");
+		System.out.println(
+				new ServiceImp().orderAirline("AirlineCode=CA1831&Company=中国国航&StartDrome=北京首都国际机场&ArriveDrome=上海虹桥国际机场&StartTime=2016-06-25 07:30&ArriveTime=2016-06-25 09:40&ck=&listTre=[{\"agency\":{\"address\":123,\"contacts\":123,\"id\":1,\"name\":132,\"phone\":23154,\"pwd\":123},\"id\":1,\"idcard\":340826199502281500,\"name\":\"石胡\",\"phone\":13027710367,\"sex\":\"男\",\"teamid\":\"1\"}]\"")
+				);
+		System.out.println("123");
+		
 	}
 }
